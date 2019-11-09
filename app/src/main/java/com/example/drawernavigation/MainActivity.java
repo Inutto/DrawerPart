0122da17a8e612bdc6155e288a253c55cc7a2a9a
Inutto <wang9550@umn.edu>
2019-11-09 12:10:37 -0600

    implement drawer and Battery Fragment. The methods for battery is implemented in MainActivity

--- app/src/main/java/com/example/drawernavigation/MainActivity.java
+++ app/src/main/java/com/example/drawernavigation/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.drawernavigation;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 
+import com.example.drawernavigation.ui.BatteryFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.os.PowerManager;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 
+import androidx.core.app.NotificationManagerCompat;
+import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,6 +32,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -77,4 +90,83 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 
+
+
+
+
+    // Battery Part
+
+    private Notification notify1;
+    private NotificationManager nManager;
+    //public static int CHANNEL_ID = 1;
+    public String CHANNEL_ID;
+
+    {
+        CHANNEL_ID = "battery_notification";
+    }
+
+    public int getBatteryLeft(){
+        IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = registerReceiver(null, filter);
+
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        float batteryP = level / (float)scale;
+        return level;
+    }
+
+    public void simulateNotification(View view){
+        Switch switchStatus = (Switch) findViewById(R.id.switchReceive);
+        if(switchStatus.isChecked()){
+            pushNotification();
+        }
+
+    }
+
+    //function of send notification to self
+    public void pushNotification(){
+        Log.d("battery","pushNotification()");
+        //nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification.Builder nBuilder = new Notification.Builder(this);
+
+        //value
+        nBuilder.setContentTitle("Battery SOS !")
+                .setContentText("Someone's phone is dying! Can you help him?")
+                .setSmallIcon(R.drawable.pic_batterysos)
+                .setTicker(",,, --- ...")
+                .setPriority(Notification.PRIORITY_MAX);
+        NotificationManagerCompat nManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notify1 = nBuilder.build();
+        nManager.notify(1,notify1);
+
+    }
+
+    public void saveBattery(View view){
+        PowerManager powerManager = (PowerManager)getSystemService(Context.POWER_SERVICE);
+        boolean powerSaveMode = powerManager.isPowerSaveMode();
+        if (powerSaveMode){
+            Log.d("battery","Power save mode is on");
+        } else {
+            Log.d("battery","Power save mode is off, trying to open it...");
+
+            //requrest permission
+
+
+            Intent battSaverIntent = new Intent();
+
+            //currently for opening the savermode's page:
+            //battSaverIntent.setComponent(new ComponentName("com.android.settings", "com.android.settings.Settings$BatterySaverSettingsActivity"));
+            //startActivityForResult(battSaverIntent, 0);
+
+            //only works on google/sansung phone I guess;
+            Intent batterySaverIntent=new Intent(Settings.ACTION_BATTERY_SAVER_SETTINGS);
+            startActivity(batterySaverIntent);
+            Log.d("battery","Finish opening power save mode");
+        }
+    }
+
 }
+
--- app/src/main/java/com/example/drawernavigation/ui/BatteryFragment.java
+++ app/src/main/java/com/example/drawernavigation/ui/BatteryFragment.java
@@ -1,33 +1,85 @@
 package com.example.drawernavigation.ui;
 
+import androidx.core.app.NotificationManagerCompat;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.PowerManager;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.drawernavigation.MainActivity;
 import com.example.drawernavigation.R;
 
 public class BatteryFragment extends Fragment {
 
-    private BatteryViewModel mViewModel;
+    /*
+
+
+
+    public static interface BatteryListener{
+
+        public int getBatteryLeft();
+
+        public void simulateNotification(View view);
+
+        public void pushNotification();
+
+        public void saveBattery(View view);
+
+    }
+
+     */
+    public MainActivity mListener;
+
 
     public static BatteryFragment newInstance() {
         return new BatteryFragment();
     }
 
+
+    @Override
+    public void onAttach(Activity activity){
+        super.onAttach(activity);
+
+        //transfer interface
+        if(activity instanceof MainActivity){
+            mListener = ((MainActivity) activity);
+        }
+    }
+
+
+
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.battery_fragment, container, false);
+        View root = inflater.inflate(R.layout.battery_fragment, container, false);
+
+        TextView batteryLeft = root.findViewById(R.id.batteryTextView);
+
+
+        batteryLeft.setText(Integer.toString(mListener.getBatteryLeft()) + "%");
+        return root;
     }
 
+    /*
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -35,4 +87,7 @@ public class BatteryFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+     */
 }
+
+
