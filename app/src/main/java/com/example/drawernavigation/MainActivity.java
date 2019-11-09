a6097a146f083fa5090c9fc0c9779f3270e50ebc
Inutto <wang9550@umn.edu>
2019-11-09 01:54:30 -0600

    Git test

--- app/src/main/java/com/example/drawernavigation/MainActivity.java
+++ app/src/main/java/com/example/drawernavigation/MainActivity.java
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //gittest
+
         /*
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
